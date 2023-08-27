704de11094a234edff10b4444ba6dbf1271c7150
aeon <yasunofabio@outlook.com.br>
2023-08-27 16:49:12 -0400

    adiciona camada de apresentacao

---
+++ src/main/java/resource/PeopleResouce.java
@@ -0,0 +1,70 @@
+package resource;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.jboss.logging.Logger;
+
+import entity.PeopleEntity;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriBuilder;
+import jakarta.ws.rs.core.UriInfo;
+import service.PeopleService;
+
+@Path("/people")
+public class PeopleResouce {
+
+	private static final Logger log = Logger.getLogger(PeopleResouce.class);
+	
+	@Inject
+	private PeopleService peopleService;
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createEnterprise(@Valid PeopleEntity enterprisePeople, @Context UriInfo uriInfo) {
+		enterprisePeople = peopleService.createPeople(enterprisePeople);
+		UriBuilder builder = uriInfo.getAbsolutePathBuilder().path(enterprisePeople.id.toString());
+		log.info("New people created with URI " + builder.build().toString());
+		return Response.created(builder.build()).build();
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllEnterprises() {
+		List<PeopleEntity> enterpriseEntityList = peopleService.findAllPeople();
+		
+		if (enterpriseEntityList.isEmpty() == false) {
+			return Response.ok(enterpriseEntityList).build();
+		} else {
+			return Response.noContent().build();
+		}
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public Response getEnterprise(@PathParam("id") String id) {
+		if (Objects.nonNull(id)) {
+			PeopleEntity enterpriseEntity = peopleService.findPeopleById(id);
+			
+			if (Objects.nonNull(enterpriseEntity)) {
+				return Response.ok(enterpriseEntity).build();
+			} else {
+				return Response.noContent().build();
+			}
+		}
+
+		return Response.status(Response.Status.BAD_REQUEST).build();
+	}
+}
